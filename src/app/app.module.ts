9cd27b81d7e8abdcf8d8c966f0924a1f302394b2
Danny Virasawmi <danv100@hotmail.com>
2019-07-02 23:05:40 -0400

    Worked up to getting secondary menu up on header.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './layout/header/header.component';
 import { FooterComponent } from './layout/footer/footer.component';
+import { AccountInfoComponent } from './layout/header/account-info/account-info.component';
+import { SecondaryNavComponent } from './layout/header/secondary-nav/secondary-nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    AccountInfoComponent,
+    SecondaryNavComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/layout/header/account-info/account-info.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-account-info',
+  templateUrl: './account-info.component.html',
+  styleUrls: ['./account-info.component.scss']
+})
+export class AccountInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  usernameActive = "Jane Doe";
+
+}
