71b9ce4434df0bef9f4995b0a3b33a94d9d7f23e
Danny Virasawmi <danv100@hotmail.com>
2019-07-03 00:39:03 -0400

    Added routing for 2 pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './components/home/home.component';
+import { BookComponent } from './components/book/book.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'book', component: BookComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, Title } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
@@ -8,6 +8,8 @@ import { HeaderComponent } from './layout/header/header.component';
 import { FooterComponent } from './layout/footer/footer.component';
 import { AccountInfoComponent } from './layout/header/account-info/account-info.component';
 import { SecondaryNavComponent } from './layout/header/secondary-nav/secondary-nav.component';
+import { HomeComponent } from './components/home/home.component';
+import { BookComponent } from './components/book/book.component';
 
 @NgModule({
   declarations: [
@@ -15,14 +17,16 @@ import { SecondaryNavComponent } from './layout/header/secondary-nav/secondary-n
     HeaderComponent,
     FooterComponent,
     AccountInfoComponent,
-    SecondaryNavComponent
+    SecondaryNavComponent,
+    HomeComponent,
+    BookComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NgbModule
   ],
-  providers: [],
+  providers: [Title],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
